fc4fa23de7a9f6593a0383302170651e7bfd98c1
NatashaSay <nataliia.saichyshyna@nure.ua>
2019-11-28 22:25:06 +0200

    Structure

--- voting/settings.py
+++ voting/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'votingsystem',
     'blockchain',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'voting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'voteDB',
+        'USER': 'postgres',
+        'PASSWORD': 'dwerty88',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
