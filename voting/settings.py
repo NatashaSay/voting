cf5a814b1b1c10d88106b4950ac82257e39b6115
NatashaSay <nataliia.saichyshyna@nure.ua>
2020-01-09 21:37:11 +0200

    Bug fixes

--- voting/settings.py
+++ voting/settings.py
@@ -151,7 +151,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "votingmain/static")
 #STATIC_ROOT = os.path.join(BASE_DIR, )
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
